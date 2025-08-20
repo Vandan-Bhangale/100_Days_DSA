600185102eab1183e82e1e857ab54d7e3febd905
Vandan-Bhangale <vandan9804@gmail.com>
2025-08-20 22:32:44 +0530

    Solved leetcode problem 155

---
+++ Stack_Queue/MinStack.java
@@ -0,0 +1,37 @@
+package Stack_Queue;
+
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        // Push onto minStack only if it's empty or the new value is <= current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int removed = stack.pop();
+        // If the removed element is the current minimum, pop from minStack too
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
